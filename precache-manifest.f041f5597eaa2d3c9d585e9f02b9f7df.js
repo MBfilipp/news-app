d56ad49f52f1b94d925f179d4c7dca8df542d07a
MBfilipp <filippstryukov@gmail.com>
2020-03-07 14:00:21 +0200

    Updates

---
+++ precache-manifest.f041f5597eaa2d3c9d585e9f02b9f7df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c50d26b084f90af3aab1e2a04ce8be9",
+    "url": "/news-app/index.html"
+  },
+  {
+    "revision": "fa9c3fe35f01be47d86f",
+    "url": "/news-app/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "04e62f61b8bee936035b",
+    "url": "/news-app/static/css/main.69bddb31.chunk.css"
+  },
+  {
+    "revision": "fa9c3fe35f01be47d86f",
+    "url": "/news-app/static/js/2.b8c082e2.chunk.js"
+  },
+  {
+    "revision": "87207bc9fa667f722ef4002dcc7edaa4",
+    "url": "/news-app/static/js/2.b8c082e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04e62f61b8bee936035b",
+    "url": "/news-app/static/js/main.9a55a0c4.chunk.js"
+  },
+  {
+    "revision": "1e38876c15ff22a74c68",
+    "url": "/news-app/static/js/runtime-main.3702f5a7.js"
+  }
+]);
